b6782f4e06b0837ad84d2153f7348f2e7203f615
peleg <peleg122@gmail.com>
2020-01-14 16:50:19 +0200

    Fruit interface

--- src/dataStructure/Fruit.java
+++ src/dataStructure/Fruit.java
@@ -1,4 +1,36 @@
 package dataStructure;
 
+import utils.Point3D;
+
+import javax.swing.*;
+
 public interface Fruit {
+    //constructor should be:
+    // fruits(double values,int type,Point3D location){
+    //   setImage("robot.png");
+    //   id++;
+    //   key = id;
+    //   init - Values , type , pos...
+    // }
+    String toJSON();
+
+    void fromJSON(String JSON);
+
+    void setValues(double values);
+
+    void setType(int type);
+
+    void setPoint3D(Point3D point);
+
+    double getValues();
+
+    int getType();
+
+    Point3D getPoint3D();
+
+    int getKey();
+
+    void setImg(ImageIcon img);
+
+    ImageIcon getImg();
 }
