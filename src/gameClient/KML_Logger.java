c7bf52a1a513ba974c764469cb627184f9f521d8
amir hoshen <mi.rosh160@gmail.com>
2020-01-19 18:22:28 +0200

    KML logger finished

--- src/gameClient/KML_Logger.java
+++ src/gameClient/KML_Logger.java
@@ -1,4 +1,109 @@
 package gameClient;
 
+import utils.Point3D;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+
 public class KML_Logger {
+
+    private int level;
+    private StringBuilder w;
+
+    /**
+     * Default constructor
+     */
+    public KML_Logger() {
+    }
+
+    public KML_Logger(int stage) {
+        this.level = stage;
+        w = new StringBuilder();
+        kmlStart();
+    }
+
+
+    public void kmlStart() {
+        w.append(
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n" +
+                        "<kml xmlns=\"http://earth.google.com/kml/2.2\">\r\n" +
+                        "  <Document>\r\n" +
+                        "    <name>" + "Game stage :" + this.level + "</name>" + "\r\n" +
+                        " <Style id=\"node\">\r\n" +
+                        "      <IconStyle>\r\n" +
+                        "        <Icon>\r\n" +
+                        "          <href>http://maps.google.com/mapfiles/kml/pal3/icon35.png</href>\r\n" +
+                        "        </Icon>\r\n" +
+                        "        <hotSpot x=\"32\" y=\"1\" xunits=\"pixels\" yunits=\"pixels\"/>\r\n" +
+                        "      </IconStyle>\r\n" +
+                        "    </Style>" +
+                        " <Style id=\"fruit-banana\">\r\n" +
+                        "      <IconStyle>\r\n" +
+                        "        <Icon>\r\n" +
+                        "          <href>http://maps.google.com/mapfiles/kml/pal5/icon49.png</href>\r\n" +
+                        "        </Icon>\r\n" +
+                        "        <hotSpot x=\"32\" y=\"1\" xunits=\"pixels\" yunits=\"pixels\"/>\r\n" +
+                        "      </IconStyle>\r\n" +
+                        "    </Style>" +
+                        " <Style id=\"fruit-apple\">\r\n" +
+                        "      <IconStyle>\r\n" +
+                        "        <Icon>\r\n" +
+                        "          <href>http://maps.google.com/mapfiles/kml/pal5/icon56.png</href>\r\n" +
+                        "        </Icon>\r\n" +
+                        "        <hotSpot x=\"32\" y=\"1\" xunits=\"pixels\" yunits=\"pixels\"/>\r\n" +
+                        "      </IconStyle>\r\n" +
+                        "    </Style>" +
+                        " <Style id=\"robot\">\r\n" +
+                        "      <IconStyle>\r\n" +
+                        "        <Icon>\r\n" +
+                        "          <href>http://maps.google.com/mapfiles/kml/pal4/icon26.png></href>\r\n" +
+                        "        </Icon>\r\n" +
+                        "        <hotSpot x=\"32\" y=\"1\" xunits=\"pixels\" yunits=\"pixels\"/>\r\n" +
+                        "      </IconStyle>\r\n" +
+                        "    </Style>"
+        );
+    }
+
+
+    public void PlaceMark(String id, Point3D location) {
+        LocalDateTime time = LocalDateTime.now();
+        w.append(
+                "    <Placemark>\r\n" +
+                        "      <TimeStamp>\r\n" +
+                        "        <when>" + time + "</when>\r\n" +
+                        "      </TimeStamp>\r\n" +
+                        "      <styleUrl>#" + id + "</styleUrl>\r\n" +
+                        "      <Point>\r\n" +
+                        "         <coordinates>" + location.x() + "," + location.y() + "</coordinates>\r\n" +
+                        "      </Point>\r\n" +
+                        "    </Placemark>\r\n"
+        );
+
+
+    }
+
+
+    public void kmlEndAndSave() {
+        w.append("  \r\n</Document>\r\n" +
+                "</kml>"
+        );
+        try {
+            File file = new File(this.level + ".kml");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(w.toString());
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
