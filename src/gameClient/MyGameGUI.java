6abc55673558349c3ea50040719e176057421ca2
amir hoshen <mi.rosh160@gmail.com>
2020-01-19 14:39:46 +0200

    move robots not finished yet

--- src/gameClient/MyGameGUI.java
+++ src/gameClient/MyGameGUI.java
@@ -1,212 +1,215 @@
 package gameClient;
 
 
-import Server.Game_Server;
-import algorithms.Graph_Algo;
-import dataStructure.DGraph;
-import dataStructure.edge_data;
-import dataStructure.node_data;
 import elements.nodeData;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 import utils.Point3D;
 import utils.StdDraw;
 
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
-public class MyGameGUI {
-    private int _numberOfRobots;
-    private int _numberOfFruits;
-    private HashMap<Integer, fruit> _allFruits;
-    private HashMap<Integer, robot> _allRobots;
-    public Game_Server _game_server;
-    private DGraph _dGraph;
-    private Graph_Algo _graph_algo;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import gameClient.*;
+import Server.Game_Server;
+import Server.game_service;
+import algorithms.Graph_Algo;
+import dataStructure.DGraph;
+import dataStructure.edge_data;
+import dataStructure.node_data;
 
 
-    MyGameGUI() {
-        _allFruits = new HashMap<>();
-        _allRobots = new HashMap<>();
-        StdDraw.clear();
-        StdDraw.enableDoubleBuffering();
-        init();
-        setGame();
-        paint();
-        paintFruits();
-        paintRobots();
-        StdDraw.show();
-    }
 
-    public void init() {
+public class MyGameGUI extends JFrame implements ActionListener, MouseListener, Runnable {
+    private  DGraph g;
+    private  game_service game;
+    private int senario;
+    private Graph_Algo ga;
+    private  HashMap<Point3D,fruit> fruits = new HashMap<>();
+    private HashMap<Integer, robot> robots = new HashMap<Integer, robot>();
+    double xMax = Double.NEGATIVE_INFINITY;
+    double xMin = Double.POSITIVE_INFINITY;
+    double yMax = Double.NEGATIVE_INFINITY;
+    double yMin = Double.POSITIVE_INFINITY;
+
+    public MyGameGUI() {
         Object[] levels = {"0", "1", "3", "4", "5", "6",
                 "7", "8", "9", "10", "11", "12",
                 "13", "14", "15", "16", "17", "18",
                 "19", "20", "21", "22", "23"};
-        int level = Integer.parseInt((String) JOptionPane.showInputDialog(null, "Choose Level: [0,23] ", "level", JOptionPane.QUESTION_MESSAGE, null, levels, null));
-
-        _game_server = (Game_Server) _game_server.getServer(level);
-        _dGraph = new DGraph(_game_server.getGraph());
-        _graph_algo = new Graph_Algo(_dGraph);
-        addFruits();
-        addRobots();
-        _numberOfFruits = _allFruits.size();
-        _numberOfRobots = _allRobots.size();
-    }
-
-    public void paint() {
-        paintEdges();
-        paintVertex();
-    }
-
-    private void paintFruits() {
-        for (fruit f : _allFruits.values()) {
-            if (f.get_type() == 1)
-                StdDraw.picture(f.get_pos().x(), f.get_pos().y(), "utils/apple.png");
-            else
-                StdDraw.picture(f.get_pos().x(), f.get_pos().y(), "utils/banana.png");
-        }
-    }
-
-    private void paintRobots() {
-        for (robot r : _allRobots.values()) {
-            StdDraw.picture(r.getPointLocation().x(), r.getPointLocation().y(), "utils/robot.png");
-        }
-    }
-
-    public void setGame() {
-        double xMax = Double.MIN_VALUE;
-        double xMin = Double.MAX_VALUE;
-        double yMax = Double.MIN_VALUE;
-        double yMin = Double.MAX_VALUE;
-        double epsilon = 0.002;
-        Iterator<node_data> it = _dGraph.getV().iterator();
-        nodeData n;
-        while (it.hasNext()) {
-            n = (nodeData) it.next();
-            if (n.getLocation().x() > xMax)
-                xMax = n.getLocation().x();
-            if (n.getLocation().y() > yMax)
-                yMax = n.getLocation().y();
-            if (n.getLocation().x() < xMin)
-                xMin = n.getLocation().x();
-            if (n.getLocation().y() < yMin)
-                yMin = n.getLocation().y();
-
-            StdDraw.setCanvasSize(800, 800);
-            StdDraw.setXscale(xMin - epsilon, xMax + epsilon);
-            StdDraw.setYscale(yMin - epsilon, yMax + epsilon);
-        }
-    }
-
-    private void paintVertex() {
-        try {
-            Collection<node_data> vertex = _dGraph.getV();
-            for (node_data a : vertex) {
-                double x = a.getLocation().x();
-                double y = a.getLocation().y();
-                StdDraw.setPenRadius(0.04);
-                StdDraw.setPenColor(StdDraw.BOOK_BLUE);
-                StdDraw.point(x, y);
-                StdDraw.setPenRadius(0.009);
-                StdDraw.setPenColor(StdDraw.BLACK);
-                StdDraw.text(x, y, "" + a.getKey());
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        String level = (String) JOptionPane.showInputDialog(null, "Choose Level: [0,23] ", "level", JOptionPane.QUESTION_MESSAGE, null, levels, null);
+        this.senario = Integer.parseInt(level);
+        StdDraw.setCanvasSize(1200, 1200);
+        StdDraw.setXscale( xMin, xMax);
+        StdDraw.setYscale( yMin, yMax);
+        initGame(this.senario);
 
     }
-
-    private void paintEdges() {
-        Collection<node_data> vertex = _dGraph.getV();
-        for (node_data a : vertex) {
-            Collection<edge_data> edge = _dGraph.getE(a.getKey());
-            for (edge_data e : edge) {
-                double s_x = a.getLocation().x();
-                double s_y = a.getLocation().y();
-                double d_x = _dGraph.getNode(e.getDest()).getLocation().x();
-                double d_y = _dGraph.getNode(e.getDest()).getLocation().y();
-                //draw the edge between two vertexes
-                StdDraw.setPenRadius(0.004);
-                StdDraw.setPenColor(StdDraw.RED);
-                StdDraw.line(s_x, s_y, d_x, d_y);
-            }
-        }
-    }
-
-    private void addFruits() {
-        Iterator<String> it = _game_server.getFruits().iterator();
-        String f;
-        int j = 0;
-        while (it.hasNext()) {
-            f = it.next();
-            try {
-                JSONObject fruity = new JSONObject(f.substring(9, f.length() - 1));
-                double x;
-                double y;
-                double v;
-                int t;
-                v = fruity.getDouble("value");
-                t = fruity.getInt("type");
-                String pos = fruity.getString("pos");
-                String[] positions = pos.split(",");
-                x = Double.parseDouble(positions[0]);
-                y = Double.parseDouble(positions[1]);
-                Point3D p = new Point3D(x, y);
-                fruit fruit = new fruit(p, v, t);
-                fruit.edgdeLocator(_dGraph);
-                _allFruits.put(j, fruit);
-                j++;
-            } catch (Exception var12) {
-                var12.printStackTrace();
-            }
-        }
-    }
-
-    private void addRobots() {
-        String info = _game_server.toString();
+    private void initGame(int gameNumber) {
+        Thread t = new Thread(this);
+        this.game = Game_Server.getServer(gameNumber); // you have [0,23] games
+        String sg = game.getGraph();
+        this.g = new DGraph(sg);
+        this.ga = new Graph_Algo(this.g);
+        String info = game.toString();
         JSONObject line;
         try {
             line = new JSONObject(info);
             JSONObject ttt = line.getJSONObject("GameServer");
             int rs = ttt.getInt("robots");
-            int fs = ttt.getInt("fruits");
+            System.out.println(info);
+            // the list of fruits should be considered in your solution
+            Iterator<String> f_iter = game.getFruits().iterator();
+            int fs=0;
+            while(f_iter.hasNext()) {
+                fs++;
+                fruit f = new fruit(f_iter.next().toString());
+                this.fruits.put(f.getLocation(), f);
+                f.edgdeLocator(g);
+            }
             for (int a = 0; a < rs; a++) {
                 if (a < fs) {
-                    _game_server.addRobot(_allFruits.get(a).get_edge().getSrc());
-                    nodeData temp = (nodeData) _dGraph.getNode(_allFruits.get(a).get_edge().getSrc());
+                    game.addRobot(fruits.get(a).getEdge().getSrc());//need pos
+                    nodeData temp = (nodeData) g.getNode(fruits.get(a).getEdge().getSrc());
                     temp.setRobot(true);
                 } else {
-                    nodeData temp = (nodeData) _dGraph.getNode(a);
+                    nodeData temp = (nodeData) g.getNode(a);
                     if (!temp.getRobot())
-                        _game_server.addRobot(a);
+                        game.addRobot(a);
                 }
             }
-            Iterator<String> it = _game_server.getRobots().iterator();
-            String r;
-            int j = 0;
-            while (it.hasNext()) {
-                r = it.next();
-                JSONObject robot = new JSONObject(r.substring(9, r.length() - 1));
-                int t;
-                t = robot.getInt("src");
-                robot robot1 = new robot(t);
-                robot1.location(_dGraph);
-                _allRobots.put(j, robot1);
-                j++;
+        }
+        catch (JSONException e) {e.printStackTrace();}
+        game.startGame();
+        t.start();
+        porpor();
+    }
+
+    private void porpor() {
+        for(Iterator<node_data> verIter=g.getV().iterator();verIter.hasNext();)
+        {
+            int point=verIter.next().getKey();
+            if(g.getNode(point).getLocation().x()>xMax)
+                xMax=g.getNode(point).getLocation().x();
+            if(g.getNode(point).getLocation().y()>yMax)
+                yMax=g.getNode(point).getLocation().y();
+            if(g.getNode(point).getLocation().x()<xMin)
+                xMin=g.getNode(point).getLocation().x();
+            if(g.getNode(point).getLocation().y()<yMin)
+                yMin=g.getNode(point).getLocation().y();
+        }
+        double epsilon=0.0025;
+        StdDraw.setCanvasSize(1200,800);
+        xMin=xMin-epsilon;xMax=xMax+epsilon;
+        yMin=yMin-epsilon/2;yMax=epsilon/2+yMax;
+        StdDraw.setXscale(xMin,xMax);
+        StdDraw.setYscale(yMin,yMax);
+    }
+
+    /*
+     * Function that draws the node.
+     */
+    public void drawGraph() {
+        if(!g.getV().isEmpty()) {
+            Iterator<node_data> nodes = g.getV().iterator();
+            while(nodes.hasNext()) {
+                node_data n = nodes.next();
+                double x = n.getLocation().x();
+                double y = n.getLocation().y();
+                StdDraw.setPenRadius(0.05);
+                StdDraw.setPenColor(StdDraw.RED);
+                StdDraw.point(x,y);
+                StdDraw.setPenColor(StdDraw.BLACK);
+                String str = n.getKey()+"";
+                StdDraw.text(x,y,str);
             }
-        } catch (
-                JSONException e) {
-            e.printStackTrace();
+
+            Iterator<node_data> allNodes = g.getV().iterator();
+            while(allNodes.hasNext()) {
+                node_data n = allNodes.next();
+                Iterator<edge_data> allEdgesOfNode = g.getE(n.getKey()).iterator();
+                while(allEdgesOfNode.hasNext()) {
+                    edge_data edges = allEdgesOfNode.next();
+                    double sx = g.getNode(edges.getSrc()).getLocation().x();
+                    double sy = g.getNode(edges.getSrc()).getLocation().y();
+                    double dx = g.getNode(edges.getDest()).getLocation().x();
+                    double dy = g.getNode(edges.getDest()).getLocation().y();
+
+                    StdDraw.setPenRadius(0.005);
+                    StdDraw.setPenColor(StdDraw.MAGENTA);
+
+                    StdDraw.line(sx,sy,dx,dy);
+
+                    StdDraw.setPenRadius(0.02);
+                    StdDraw.setPenColor(StdDraw.CYAN);
+
+                    double arrowX= (dx*8+sx)/9;
+                    double arrowY= (dy*8+sy)/9;
+                    StdDraw.point(arrowX,arrowY);
+
+                    String te = String.format("%.4g%n",edges.getWeight());
+
+                    StdDraw.setPenRadius(0.15);
+                    StdDraw.setPenColor(Color.BLACK);
+
+                    double newX= (dx*4+sx)/5;
+                    double newY= (dy*4+sy)/5;
+
+                    StdDraw.text(newX, newY, te);
+                }
+            }
+
         }
     }
-    //-------------Test Movement-----------------------------------------------//
-    private static void moveRobots(Game_Server game, DGraph gg) {
+    public void drawRobot() {
+        List<String> log = game.getRobots();
+        Iterator<String> rob = log.iterator();
+        while(rob.hasNext()) {
+            String Json = rob.next();
+            robot robot1 = new robot(Json);
+            robots.put(robot1.getId(), robot1);
+            StdDraw.picture(robot1.getLocation().x(), robot1.getLocation().y(), "robot1.png", 0.001, 0.001);
+        }
+    }
+    public void drawFruit() {
+        fruits.clear();
+        Iterator<String> fruit = game.getFruits().iterator();
+        while(fruit.hasNext()) {
+            fruit cf = new fruit(fruit.next());
+            this.fruits.put(cf.getLocation(), cf);
+            if(cf.getType() == 1) {
+                StdDraw.picture(cf.getLocation().x(), cf.getLocation().y(), "banana.png", 0.0008, 0.0008);
+            }
+            else {
+                StdDraw.picture(cf.getLocation().x(), cf.getLocation().y(), "apple.png", 0.0007, 0.0007);
+            }
+        }
+    }
+    private  int nextNode(DGraph g, int src) {
+        int ans = -1;
+
+        Collection<edge_data> ee = g.getE(src);
+        Iterator<edge_data> itr = ee.iterator();
+        int s = ee.size();
+        int r = (int)(Math.random()*s);
+        int i=0;
+        while(i<r) {itr.next();i++;}
+        ans = itr.next().getDest();
+        return ans;
+
+    }
+    private void moveRobots(game_service game2, DGraph gg) {
         List<String> log = game.move();
         if(log!=null) {
             long t = game.timeToEnd();
@@ -220,7 +223,7 @@ public class MyGameGUI {
                     int dest = ttt.getInt("dest");
 
                     if(dest==-1) {
-                        dest = nextNode(gg, src);
+                        dest = src+1%g.nodeSize();//nextNode(gg, src);
                         game.chooseNextEdge(rid, dest);
                         System.out.println("Turn to node: "+dest+"  time to end:"+(t/1000));
                         System.out.println(ttt);
@@ -230,27 +233,70 @@ public class MyGameGUI {
             }
         }
     }
-    private static int nextNode(DGraph g, int src) {
-        int ans = -1;
-        Collection<edge_data> ee = g.getE(src);
-        Iterator<edge_data> itr = ee.iterator();
-        int s = ee.size();
-        int r = (int)(Math.random()*s);
-        int i=0;
-        while(i<r) {itr.next();i++;}
-        ans = itr.next().getDest();
-        return ans;
-    }
-
-
-    public static void main(String[] args) {
-        MyGameGUI g = new MyGameGUI();
-        g._game_server.startGame();
-        while(g._game_server.isRunning()) {
-            moveRobots(g._game_server,g._dGraph);
-            //need to update the fruits and move robot functions
+    public void drawScore() {
+        double temp = xMax - xMin;
+        double tmp = yMax - yMin;
+        int score = 0;
+        String time =  game.timeToEnd() / 1000 + "." + game.timeToEnd() % 1000;
+        String results = game.toString();
+        try {
+            JSONObject scoreobj = new JSONObject(results);
+            JSONObject ttt = scoreobj.getJSONObject("GameServer");
+            score = ttt.getInt("grade");
+            StdDraw.text(xMin+temp/1.2, yMin+tmp/1.3,"Time: " + time );
+            StdDraw.text(xMin+temp/1.2, yMin+tmp/1.2, "Score: " + score);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed");
         }
-        String results = g._game_server.toString();
+    }
+    public void refreshDraw() {
+        StdDraw.clear();
+        drawGraph();
+    }
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public void run() {
+        while (game.isRunning()) {
+            StdDraw.enableDoubleBuffering();
+            refreshDraw();
+            drawGraph();
+            drawFruit();
+            drawRobot();
+            drawScore();
+            moveRobots(this.game, this.g);
+            StdDraw.show();
+        }
+        String results = game.toString();
         System.out.println("Game Over: "+results);
+    }
+    public static void main(String[] args) {
+        MyGameGUI junior = new MyGameGUI();
     }
 }
